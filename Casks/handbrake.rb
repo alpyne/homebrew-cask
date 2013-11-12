53d0fc74ec7e386d5de8b91351d4d04193567042
alpyne <alan.pyne+github@gmail.com>
2013-11-12 22:13:38 +0100

    Correct handbrake checksum

--- Casks/handbrake.rb
+++ Casks/handbrake.rb
@@ -1,7 +1,7 @@
 class Handbrake < Cask
-  url 'http://handbrake.fr/rotation.php?file=HandBrake-0.9.9-MacOSX.6_GUI_x86_64.dmg'
+  url 'http://downloads.sourceforge.net/project/handbrake/0.9.9/HandBrake-0.9.9-MacOSX.6_GUI_x86_64.dmg'
   homepage 'http://handbrake.fr/'
   version '0.9.9'
-  sha1 '9b6f0259f3378b0cc136378d7859162aa95c0eb7'
+  sha1 'da39a3ee5e6b4b0d3255bfef95601890afd80709'
   link 'HandBrake.app'
 end
